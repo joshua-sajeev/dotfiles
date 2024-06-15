3209fb17ca6d66b0e4d6aadf5c2eaf1030382ce2
Joshua Sajeev <joshuasajeevnv@gmail.com>
2024-06-15 09:47:37 +0530

    After nvim

--- fish/conf.d/rustup.fish
+++ fish/conf.d/rustup.fish
@@ -1,1 +1,1 @@
-#source "$HOME/.cargo/env.fish"
+#
--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/bin $PATH"
 set -g fish_greeting
 
 
