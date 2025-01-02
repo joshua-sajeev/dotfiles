472aed270075ecad2eb04d7af38df999b80b482f
Joshua Sajeev <joshuasajeevnv@gmail.com>
2025-01-02 07:55:48 +0530

    Before nvim completion setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,17 @@
-print("Advent of neovim")
 require("config.lazy")
-
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("n", "<space>pv", ":Ex<CR>")
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+vim.opt.shiftwidth     = 4
+vim.opt.tabstop        = 4
+vim.opt.number         = true
+vim.opt.relativenumber = true
+vim.opt.clipboard      = "unnamedplus"
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
---
+++ nvim/lua/config/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+	-- amongst your other plugins
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		opts = {
+			direction = 'float',
+			open_mapping = [[<a-]>]],
+		}
+	}
+}
