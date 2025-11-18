25b52a1744887b2ce32087e88e4070c02acb3202
Joshua Sajeev <joshuasajeevnv@gmail.com>
2025-11-18 17:46:00 +0530

    Daily auto update: 2025-11-18 17:46:00
    Daily auto update: 2025-11-19 08:20:55
    
    Daily auto update: 2025-12-01 20:15:52
    
    Daily auto update: 2025-12-02 19:32:49
    
    Daily auto update: 2025-12-05 20:07:59
    
    Daily auto update: 2025-12-06 16:52:00

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,13 @@ zoxide init fish | source
 export NVM_DIR="$HOME/.nvm"
 
 # Created by `pipx` on 2025-07-28 15:42:43
-set PATH $PATH /home/joshua/.local/bin
+# --- NVM PATH ---
+set -gx PATH $HOME/.local/share/nvm/v25.2.1/bin $PATH
+
+# --- Local binaries ---
+set -gx PATH $HOME/.local/bin $PATH
+
+# --- Go environment ---
+set -gx GOPATH $HOME/go
+set -gx GOBIN $GOPATH/bin
+set -gx PATH $PATH $GOBIN
